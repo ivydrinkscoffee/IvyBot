b2b9484db55254e11a8504d306e104057771b248
ivy <67686618+Ivy-Wusky@users.noreply.github.com>
2020-10-05 16:19:58 +0200

    Update Constants.cs

--- IvyBot/IvyBot/Constants.cs
+++ IvyBot/IvyBot/Constants.cs
@@ -4,6 +4,7 @@ namespace IvyBot
     {
         public const string BotToken = "Token";
         public const string LavaHost = "Host";
+        public const string LavaPort = "Port";
         public const string LavaPassword = "Password";
     }
 }
