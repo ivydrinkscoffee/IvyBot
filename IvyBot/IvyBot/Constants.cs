e8802e5eaece243157de6a25e80ebb6f880bbe9d
ivy <67686618+Ivy-Wusky@users.noreply.github.com>
2020-10-05 08:24:49 +0200

    Update Constants.cs

--- IvyBot/IvyBot/Constants.cs
+++ IvyBot/IvyBot/Constants.cs
@@ -4,7 +4,6 @@ namespace IvyBot
     {
         public const string BotToken = "Token";
         public const string LavaHost = "Host";
-        public const int LavaPort = "Port";
         public const string LavaPassword = "Password";
     }
 }
