480cd4b5088fae4a92453c80454306caa7cd3bcd
Jaime Ramos Montero <ramosmonterojaime@gmail.com>
2020-10-12 18:10:56 +0200

    Update

--- IvyBot/IvyBot/Services/NSFWService.cs
+++ IvyBot/IvyBot/Services/NSFWService.cs
@@ -10,7 +10,7 @@ namespace IvyBot.Services
         {
             try
             {
-                var rng = new Random();
+                var random = new Random();
                 var url = $"http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={tag.Replace(" ", "_")}";
                 var webpage = await SearchService.GetResponseStringAsync(url).ConfigureAwait(false);
                 var matches = Regex.Matches(webpage, "file_url=\"(?<url>.*?)\"");
@@ -18,12 +18,12 @@ namespace IvyBot.Services
                 if (matches.Count == 0)
                 return null;
                 
-                var match = matches[rng.Next(0, matches.Count)];
-                return matches[rng.Next(0, matches.Count)].Groups["url"].Value;
+                var match = matches[random.Next(0, matches.Count)];
+                return matches[random.Next(0, matches.Count)].Groups["url"].Value;
             }
             catch (Exception ex)
             {
-                // Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
                 return $"Error in rule34 search: {ex.Message}";
             }
         }
