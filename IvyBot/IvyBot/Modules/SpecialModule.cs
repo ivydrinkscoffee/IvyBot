3f7db4f44e0f7423c5b6f1cb7b92b34d7def1e58
ivy <67686618+Ivy-Wusky@users.noreply.github.com>
2020-10-13 21:50:46 +0200

    Update SpecialModule.cs

--- IvyBot/IvyBot/Modules/SpecialModule.cs
+++ IvyBot/IvyBot/Modules/SpecialModule.cs
@@ -17,6 +17,15 @@ namespace IvyBot.Modules
             await Context.Channel.SendFileAsync(stream, "5.3.0public.pchtxt");
         }
 
+        [Command("310starlion", RunMode = RunMode.Async)]
+        [Summary("Sends the latest public Starlion for Splatoon 2")]
+        public async Task SendStarlionAsync()
+        {
+            var filestream = WebRequest.Create("https://splatoon-hackers.github.io/starlion_public.rar");
+            Stream stream = filestream.GetResponse().GetResponseStream();
+            await Context.Channel.SendFileAsync(stream, "starlion_public.rar");
+        }
+
         [Command("color", RunMode = RunMode.Async)]
         [Summary("Sends an image of the color you have requested in hex")]
         public async Task ColorViewer([Remainder] string hex)
