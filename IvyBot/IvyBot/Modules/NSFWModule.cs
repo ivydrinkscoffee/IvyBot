98565facff52b3d94256d99aad41e1ddfe11bbfb
ivy <67686618+Ivy-Wusky@users.noreply.github.com>
2020-11-09 20:26:52 +0100

    Update NSFWModule.cs

--- IvyBot/IvyBot/Modules/NSFWModule.cs
+++ IvyBot/IvyBot/Modules/NSFWModule.cs
@@ -48,7 +48,7 @@ namespace IvyBot.Modules
                 }
                 else
                 {
-                    await ReplyAsync(link)
+                    await ReplyAsync(link);
                 }
             }
             else
