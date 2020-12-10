107d40f57b81a2b433a0a5c8b92c555bc4da143d
ivy <67686618+Ivy-Wusky@users.noreply.github.com>
2020-12-10 14:26:05 +0100

    Tabs suck 2: electric boogaloo

--- IvyBot/IvyBot/Services/ColorService.cs
+++ IvyBot/IvyBot/Services/ColorService.cs
@@ -6,44 +6,44 @@ using IvyBot.Modules;
 
 namespace IvyBot.Services
 {
-	public class ColorService
-	{
-		public class ColorDef
-		{
-			public string Id;
-			public string Name;
-			public Color Color;
-			
-			public ColorDef(string name, Color color)
-			{
-				Name = name;
-				Id = name.ToLowerInvariant();
-				Color = color;
-			}
-		}
-        
-		public static async Task<string> SetColorAsync(SocketGuildUser user, string colorName)
-		{
-			ColorDef color;
-			
-			if (!ColorModule.colorMap.TryGetValue(colorName.ToLowerInvariant(), out color))
-			{
-				return "Unknown color";
-			}
-
-			SocketRole role = user.Guild.Roles.Where(r => r.Name == color.Name).FirstOrDefault();
-
-			if (role == null)
-			{
-				var newRole = await user.Guild.CreateRoleAsync(color.Name, permissions: GuildPermissions.None, color: color.Color, false, null);
-				await user.AddRoleAsync(newRole);
-
-				return $"Set **{user.ToString()}**'s color role to **{color.Name}**";
-			}
-
-			await user.AddRoleAsync(role);
+    public class ColorService
+    {
+        public class ColorDef
+        {
+            public string Id;
+            public string Name;
+            public Color Color;
             
-			return $"Set **{user.ToString()}**'s color role to **{color.Name}**";
-		}
-	}
+            public ColorDef(string name, Color color)
+            {
+                Name = name;
+                Id = name.ToLowerInvariant();
+                Color = color;
+            }
+        }
+        
+        public static async Task<string> SetColorAsync(SocketGuildUser user, string colorName)
+        {
+            ColorDef color;
+            
+            if (!ColorModule.colorMap.TryGetValue(colorName.ToLowerInvariant(), out color))
+            {
+                return "Unknown color";
+            }
+
+            SocketRole role = user.Guild.Roles.Where(r => r.Name == color.Name).FirstOrDefault();
+
+            if (role == null)
+            {
+                var newRole = await user.Guild.CreateRoleAsync(color.Name, permissions: GuildPermissions.None, color: color.Color, false, null);
+                await user.AddRoleAsync(newRole);
+
+                return $"Set **{user.ToString()}**'s color role to **{color.Name}**";
+            }
+
+            await user.AddRoleAsync(role);
+            
+            return $"Set **{user.ToString()}**'s color role to **{color.Name}**";
+        }
+    }
 }
