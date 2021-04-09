87d9a39883722eaa0e8b3b8ea6021bc6021e9a48
Ivy <67686618+ivydrinkscoffee@users.noreply.github.com>
2021-04-10 01:05:17 +0200

    Alert the owner when the bot joins a new server

--- IvyBot/IvyBot/CommandHandler.cs
+++ IvyBot/IvyBot/CommandHandler.cs
@@ -28,9 +28,10 @@ namespace IvyBot {
 
             _cmdService.Log += LogAsync;
             _client.MessageReceived += HandleMessageAsync;
+            _client.JoinedGuild += HandleGuildJoinedAsync;
 
-            // _client.MessageUpdated += HandleUpdatedMessageAsync;
-            // _client.MessageDeleted += HandleDeletedMessageAsync;
+                // _client.MessageUpdated += HandleUpdatedMessageAsync;
+                // _client.MessageDeleted += HandleDeletedMessageAsync;
         }
 
         private async Task HandleMessageAsync (SocketMessage socketMessage) {
@@ -55,114 +56,104 @@ namespace IvyBot {
                     case CommandError.UnknownCommand:
                         return;
                     default:
-                        await context.Channel.SendMessageAsync ($"{result.ErrorReason}");
+                        await context.Channel.SendMessageAsync (result.ErrorReason);
                         break;
                 }
             }
         }
 
         /*
-        private async Task HandleUpdatedMessageAsync(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
-        {
-            ulong logChannelId = 771314761807691808;
-            var logChannel = _client.GetChannel(logChannelId) as ISocketMessageChannel;
-            
-            try
-            {
-                var message = await before.GetOrDownloadAsync();
-                
-                if (after.Author.IsBot == true || after is null)
-                {
+        private async Task HandleUpdatedMessageAsync (Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel) {
+            ulong logChannelId;
+            var logChannel = _client.GetChannel (logChannelId) as ISocketMessageChannel;
+
+            try {
+                var message = await before.GetOrDownloadAsync ();
+
+                if (after.Author.IsBot == true || after is null) {
                     return;
-                }
-                else
-                {
+                } else {
                     string channelName = channel.Name;
 
                     var author = after.Author as SocketUser;
-                    var avatar = author.GetAvatarUrl(ImageFormat.Auto);
-                    var defaultAvatar = author.GetDefaultAvatarUrl();
+                    var avatar = author.GetAvatarUrl (ImageFormat.Auto);
+                    var defaultAvatar = author.GetDefaultAvatarUrl ();
 
-                    EmbedAuthorBuilder embedAuthorBuilder = new EmbedAuthorBuilder();
+                    EmbedAuthorBuilder embedAuthorBuilder = new EmbedAuthorBuilder ();
 
-                    embedAuthorBuilder.WithName($"Message edited by {author.Username}#{author.Discriminator} in #{channelName}");
-            
-                    if (avatar == null)
-                    {
-                        embedAuthorBuilder.WithIconUrl(defaultAvatar);
-                    }
-                    else
-                    {
-                        embedAuthorBuilder.WithIconUrl(avatar);
+                    embedAuthorBuilder.WithName ($"Message edited by {author.Username}#{author.Discriminator} in #{channelName}");
+
+                    if (avatar == null) {
+                        embedAuthorBuilder.WithIconUrl (defaultAvatar);
+                    } else {
+                        embedAuthorBuilder.WithIconUrl (avatar);
                     }
 
-                    EmbedBuilder builder = new EmbedBuilder();
+                    EmbedBuilder builder = new EmbedBuilder ();
 
-                    builder.AddField("Before", $"```diff\n- {message}\n```", true);
-                    builder.AddField("After", $"```diff\n+ {after}\n```", true);
-                    builder.WithAuthor(embedAuthorBuilder);
-                    builder.WithCurrentTimestamp();
-                    builder.WithColor(Color.Blue);
+                    builder.AddField ("Before", $"```diff\n- {message}\n```", true);
+                    builder.AddField ("After", $"```diff\n+ {after}\n```", true);
+                    builder.WithAuthor (embedAuthorBuilder);
+                    builder.WithCurrentTimestamp ();
+                    builder.WithColor (Color.Blue);
 
-                    await logChannel.SendMessageAsync("", false, builder.Build());
+                    await logChannel.SendMessageAsync ("", false, builder.Build ());
                 }
-            }
-            catch (Exception ex)
-            {
-                await logChannel.SendMessageAsync(ex.Message);
+            } catch (Exception ex) {
+                await logChannel.SendMessageAsync (ex.Message);
             }
         }
 
-        private async Task HandleDeletedMessageAsync(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel)
-        {
-            ulong logChannelId = 771314761807691808;
-            var logChannel = _client.GetChannel(logChannelId) as ISocketMessageChannel;
-            
-            try
-            {
-                var originalMessage = await message.GetOrDownloadAsync();
-                
-                if (originalMessage.Author.IsBot == true || originalMessage is null)
-                {
+        private async Task HandleDeletedMessageAsync (Cacheable<IMessage, ulong> message, ISocketMessageChannel channel) {
+            ulong logChannelId;
+            var logChannel = _client.GetChannel (logChannelId) as ISocketMessageChannel;
+
+            try {
+                var originalMessage = await message.GetOrDownloadAsync ();
+
+                if (originalMessage.Author.IsBot == true || originalMessage is null) {
                     return;
-                }
-                else
-                {
+                } else {
                     string channelName = channel.Name;
-                    
+
                     var author = originalMessage.Author as SocketUser;
-                    var avatar = author.GetAvatarUrl(ImageFormat.Auto);
-                    var defaultAvatar = author.GetDefaultAvatarUrl();
+                    var avatar = author.GetAvatarUrl (ImageFormat.Auto);
+                    var defaultAvatar = author.GetDefaultAvatarUrl ();
 
-                    EmbedAuthorBuilder embedAuthorBuilder = new EmbedAuthorBuilder();
+                    EmbedAuthorBuilder embedAuthorBuilder = new EmbedAuthorBuilder ();
 
-                    embedAuthorBuilder.WithName($"Message deleted by {author.Username}#{author.Discriminator} in #{channelName}");
-            
-                    if (avatar == null)
-                    {
-                        embedAuthorBuilder.WithIconUrl(defaultAvatar);
-                    }
-                    else
-                    {
-                        embedAuthorBuilder.WithIconUrl(avatar);
+                    embedAuthorBuilder.WithName ($"Message deleted by {author.Username}#{author.Discriminator} in #{channelName}");
+
+                    if (avatar == null) {
+                        embedAuthorBuilder.WithIconUrl (defaultAvatar);
+                    } else {
+                        embedAuthorBuilder.WithIconUrl (avatar);
                     }
 
-                    EmbedBuilder builder = new EmbedBuilder();
+                    EmbedBuilder builder = new EmbedBuilder ();
 
-                    builder.AddField("Message", $"```diff\n- {originalMessage}\n```", true);
-                    builder.WithAuthor(embedAuthorBuilder);
-                    builder.WithCurrentTimestamp();
-                    builder.WithColor(Color.Blue);
+                    builder.AddField ("Message", $"```diff\n- {originalMessage}\n```", true);
+                    builder.WithAuthor (embedAuthorBuilder);
+                    builder.WithCurrentTimestamp ();
+                    builder.WithColor (Color.Blue);
 
-                    await logChannel.SendMessageAsync("", false, builder.Build());
+                    await logChannel.SendMessageAsync ("", false, builder.Build ());
                 }
-            }
-            catch (Exception ex)
-            {
-                await logChannel.SendMessageAsync(ex.Message);
+            } catch (Exception ex) {
+                await logChannel.SendMessageAsync (ex.Message);
             }
         }
         */
+
+        private async Task HandleGuildJoinedAsync (SocketGuild socketGuild) {
+            var invites = await socketGuild.GetInvitesAsync();
+            foreach (var invite in invites) {
+                if (invite != null) {
+                    var info = await _client.GetApplicationInfoAsync();
+                    await info.Owner.SendMessageAsync(invite.ToString());
+                }
+            }
+        }
 
         private async Task<Task> LogAsync (LogMessage logMessage) {
             await _logService.LogAsync (logMessage);
